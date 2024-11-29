ee515113d324feade4f5c904196453abedf0378e
Lior Ifrach <lior.ifrach@post.runi.ac.il>
2024-11-29 13:27:07 +0200

    anagram added

--- Anagram.java
+++ Anagram.java
@@ -2,27 +2,27 @@
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
-		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
-		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
-
-		// Tests the preProcess function.
+		System.out.println(isAnagram("silent", "listen"));  // true
+		System.out.println(isAnagram("William Shakespeare", "I am a weakish speller")); // true
+		System.out.println(isAnagram("Madam Curie", "Radium came")); // true
+		System.out.println(isAnagram("Tom Marvolo Riddle", "I am Lord Voldemort")); // true
+	
 		System.out.println(preProcess("What? No way!!!"));
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		// Performs a stress test of randomAnagram 
+	
+		System.out.print("silent and ");
+		System.out.print(randomAnagram("silent"));
+		System.out.println(" are anagrams.");
+	
 		String str = "1234567";
 		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
 		for (int i = 0; i < 10; i++) {
 			String randomAnagram = randomAnagram(str);
-			System.out.println(randomAnagram);
 			pass = pass && isAnagram(str, randomAnagram);
 			if (!pass) break;
 		}
 		System.out.println(pass ? "test passed" : "test Failed");
-	}  
+	}
+	 
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
@@ -62,9 +62,10 @@ public class Anagram {
 		int n = str.length();
 		String newstr = "";
 		for (int i = 0; i < n; i++) {
-			if (str.charAt(i) > 96 && str.charAt(i) < 123) {
-				newstr = newstr + str.charAt(i);
-			}
+			char c = str.charAt(i);
+			if (c >= 'a' && c <= 'z') {
+				newstr += c;
+			} 
 		}
 		return newstr;
 	}
